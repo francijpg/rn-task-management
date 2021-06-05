dfdbb10c5b217bfe9e093f9b47debc10cc3949ed
francijpg <hellofrancijpg@gmail.com>
2021-06-05 08:50:56 -0400

    Update apollo.js

--- src/config/apollo.js
+++ src/config/apollo.js
@@ -1,13 +1,27 @@
 import {ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client';
 import {Platform} from 'react-native';
+import {setContext} from 'apollo-link-context';
+import {getToken} from '../utils/token';
 
 const uri =
   Platform.OS === 'ios' ? 'http://localhost:4000' : 'http://10.0.2.2:4000/';
 const httpLink = createHttpLink({uri});
 
+const authLink = setContext(async (_, {headers}) => {
+  const token = await getToken();
+
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  // connectToDevTools: true,
   cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
 });
 
 export default client;
