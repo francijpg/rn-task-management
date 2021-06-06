ee8a781602dbce9b48ff1963a040c063fb3a1d87
francijpg <hellofrancijpg@gmail.com>
2021-06-06 09:48:48 -0400

    Create project.js

---
+++ src/views/project.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text} from 'native-base';
+// import globalStyles from '../styles/global';
+
+const Project = () => {
+  return <Text>task management</Text>;
+};
+
+export default Project;
